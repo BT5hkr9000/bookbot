82b062bf39c1330e14d7853f47d2573f3907e841
BT5hkr9000 <j-boldwin@live.co.uk>
2025-01-10 15:55:00 +0000

    Added sorted and formatted character counts

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     print(text)
     count_words(text)
-    print(count_characters(text))
+    count_characters(text)
 
 
 def get_book_text(path):
@@ -26,6 +26,19 @@ def count_characters(book):
                 char_count[letter] += 1
             else:
                  char_count[letter] = 1
-    return char_count
+    
+    char_list = []
+    for letter in char_count:
+        char_list.append({'letter': letter, 'count': char_count[letter]})
+
+    def sort_on(dict):
+        return dict['count']
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    for char_dict in char_list:
+        #count = char_count[letter]
+        #print(char_list[1])
+        print(f"The '{char_dict['letter']}' character was found {char_dict['count']} times")
 
 main()
